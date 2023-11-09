e66b5cde30aa51216f7d4c54eb4c3718e26de816
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-09 11:37:26 +0530

    updated crud with search

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('profile/<str:pk>', views.profilePage,name='profile'),
     path('create-room/', views.createRoom,name='create-room'),
     path('update-room/<str:pk>', views.updateRoom,name='update-room'),
+    path('delete-room/<str:pk>', views.deleteRoom,name='delete-room'),
 ]

193dfbb0d5ee3056995bf1df0f6eb0b108944749
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-10 13:15:16 +0530

    activity feed

--- base/models.py
+++ base/models.py
@@ -13,7 +13,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants = 
+    participants = models.ManyToManyField(User, related_name='participants',blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateField(auto_now_add=True)
     
@@ -30,7 +30,10 @@ class Message(models.Model):
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        ordering = ['-updated','-created']
     
     def __str__(self):
         return self.body[:50]
--- base/views.py
+++ base/views.py
@@ -67,22 +67,27 @@ def homePage(request):
     )
     room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {'rooms':rooms,'topics':topics,'room_count':room_count}
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
+    
+    
+    context = {'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
     return render(request, 'base/home.html',context)
 
 def roomPage(request,pk):
     room = Room.objects.get(id=pk)
-    roommessages = room.message_set.all().order_by('-created')
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
     
     if request.method == 'POST':
         message = Message.objects.create(
             user = request.user,
-            room=room
-            body=request.get('body')
+            room=room,
+            body=request.POST.get('body')
         )
+        room.participants.add(request.user)
         return redirect('room',pk=room.id)
     
-    context = {'room':room,'roommessages':roommessages}
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html',context)
 
 @login_required(login_url='login')
@@ -123,6 +128,20 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     context = {'obj':room}
+    return render(request,'base/delete.html',context)
+
+# delete message
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to delete')
+    
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    context = {'obj':message}
     return render(request,'base/delete.html',context)
         
 # profile poge

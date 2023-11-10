4b0594f824edcb8d8c3274b0951c04c3aa660a2d
Suman Medda <sumanmedda@Sumans-MacBook-Air.local>
2023-11-10 21:23:25 +0530

    theme install and making data dynamic

--- base/models.py
+++ base/models.py
@@ -15,7 +15,7 @@ class Room(models.Model):
     description = models.TextField(null=True, blank=True)
     participants = models.ManyToManyField(User, related_name='participants',blank=True)
     updated = models.DateTimeField(auto_now=True)
-    created = models.DateField(auto_now_add=True)
+    created = models.DateTimeField(auto_now_add=True)
     
     class Meta:
         ordering = ['-updated','-created'] 
